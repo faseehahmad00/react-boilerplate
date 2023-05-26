14ea5a79c75a00f6eedcb8bf0d33ca324d9d14d0
Faseeh Ahmad <faseehahmad00@gmail.com>
2023-05-26 06:55:47 +0500

    PWA Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        id:"/",
+        name: 'Test',
+        short_name: 'Test',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
